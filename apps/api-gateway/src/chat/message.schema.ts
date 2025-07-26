ee196b4dc0c7a23f4242d93f30b0bfd79280a7f8
MasonS <amsadeghi79@gmail.com>
2025-07-26 22:11:13 +0330

    Update package dependencies and add ESLint and Prettier configurations

---
+++ apps/api-gateway/src/chat/message.schema.ts
@@ -0,0 +1,17 @@
+// src/chat/message.schema.ts
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Message extends Document {
+  @Prop({ required: true })
+  senderId: string;
+
+  @Prop({ required: true })
+  receiverId: string;
+
+  @Prop({ required: true })
+  message: string;
+}
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
