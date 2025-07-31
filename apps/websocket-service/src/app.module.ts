e63732e3e7844b33ba26e9f578bcec2dc761c92c
MasonS <amsadeghi79@gmail.com>
2025-07-31 23:44:35 +0330

    update app.module for websocket-service

--- apps/websocket-service/src/app.module.ts
+++ apps/websocket-service/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ChatGateway } from './chat/chat.gateway';
+import { PresenceService } from './presence/presence.service';
+import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
+import { RedisModule } from './redis/redis.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    RabbitMQModule,
+    RedisModule,
+  ],
+  providers: [
+    ChatGateway,
+    PresenceService,
+    AuthService,
+  ],
 })
 export class AppModule {}
