bc07c3b7057987d0b57c6e54cee55c95e94e67ae
MasonS <amsadeghi79@gmail.com>
2025-08-07 22:59:27 +0330

    update event.module.ts

--- apps/websocket-service/src/events/events.module.ts
+++ apps/websocket-service/src/events/events.module.ts
@@ -4,9 +4,11 @@ import { ChatHandler } from './handlers/chat.handler';
 import { MessageHandler } from './handlers/message.handler';
 import { RedisModule } from '../redis/redis.module';
 import { ChatModule } from '../chat/chat.module';
+import { PresenceModule } from '../presence/presence.module';
+import { MetricsModule } from '../metrics/metrics.module';
 
 @Module({
-  imports: [RedisModule, ChatModule],
+  imports: [RedisModule, ChatModule, PresenceModule, MetricsModule],
   providers: [EventListenerService, ChatHandler, MessageHandler],
   exports: [EventListenerService],
 })
