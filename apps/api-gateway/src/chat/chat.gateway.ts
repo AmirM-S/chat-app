8dc57d966a654844a4fd1727a8ee34dc81c4345b
MasonS <amsadeghi79@gmail.com>
2025-08-26 23:38:25 +0330

    update chat.gateway.ts

--- apps/api-gateway/src/chat/chat.gateway.ts
+++ apps/api-gateway/src/chat/chat.gateway.ts
@@ -1,14 +1,3 @@
-// import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-
-// @WebSocketGateway()
-// export class ChatGateway {
-//   @SubscribeMessage('message')
-//   handleMessage(client: any, payload: any): string {
-//     return 'Hello world!';
-//   }
-// }
-
-// src/chat.gateway.ts
 import {
   WebSocketGateway,
   WebSocketServer,
